65bf7340dbfd8c9dbab822b7e9bd62e7b634454b
eltoncampos1 <eltoncampos36@gmail.com>
2021-10-27 15:48:37 -0300

    feat: At the end of each question, display the answers.

--- src/components/Questions/index.tsx
+++ src/components/Questions/index.tsx
@@ -1,12 +1,12 @@
-import { Button, Typography } from "@mui/material"
-import { Box  } from "@mui/system";
-import { useEffect, useState } from "react";
-import {getQuestions} from '../../services/getQuestions'
-import { useQuestions } from "../../context/questions";
-import { useFetchQuestionsByQuantity } from "../hooks/useFetchQuestionsByQuantity";
-import { Question } from "./Question";
+import { Button, Typography } from '@mui/material';
+import { Box } from '@mui/system';
+import { useEffect, useState } from 'react';
+import { getQuestions } from '../../services/getQuestions';
+import { useQuestions } from '../../context/questions';
+import { useFetchQuestionsByQuantity } from '../hooks/useFetchQuestionsByQuantity';
+import { Question } from './Question';
 
-interface  QuestionProps {
+interface QuestionProps {
     category: string;
     question: string;
     difficulty: string;
@@ -14,50 +14,42 @@ interface  QuestionProps {
     type?: string;
 }
 
-
-
 export const Questions = () => {
-  const {formValues} = useQuestions()
-  const [quantity, setQuantity] = useState(Number(formValues.numberofQuestions))
-  const [questions, setQuestions] = useState<Array<QuestionProps>>([] as QuestionProps)
-  const [loading, setLoading] = useState(false)
+  const { formValues } = useQuestions();
+  const [quantity, setQuantity] = useState(Number(formValues.numberofQuestions));
+  const [questions, setQuestions] = useState<Array<QuestionProps>>([] as QuestionProps);
+  const [loading, setLoading] = useState(false);
 
-  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [currentQuestion, setCurrentQuestion] = useState(0);
 
   const NextQuestion = () => {
+    setCurrentQuestion((prevQuestion) => prevQuestion + 1);
+  };
 
-    setCurrentQuestion((prevQuestion) => prevQuestion + 1)
-  }
+  console.log(questions);
 
-
-  console.log(currentQuestion);
-  
-
-  const fetchQuestions = async() => {
-    const data = await getQuestions(quantity)
-    setQuestions(data.results)
-    return data
-  }
+  const fetchQuestions = async () => {
+    const data = await getQuestions(quantity);
+    setQuestions(data.results);
+    return data;
+  };
 
   useEffect(() => {
-    fetchQuestions()
-  },[])
-   
+    fetchQuestions();
+  }, []);
+
   return (
     <Box
       sx={{
-          width: "100%",
-          flexDirection: "column",
-          height: '100vh',
-          display: 'flex', 
-          alignItems: 'center', 
-          justifyContent: 'center', 
+        width: '100%',
+        flexDirection: 'column',
+        height: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
       }}
     >
-          <Question questions={questions}/>
+      <Question questions={questions} />
     </Box>
-  )
-    
-
-      
-}
+  );
+};
--- src/context/questions.tsx
+++ src/context/questions.tsx
@@ -1,40 +1,42 @@
-import { createContext, useContext, useState } from "react";
-import { Steps } from "../screens/QuestionsLayout/types";
-import {IDefaultValues, IQuestionContext, QuestionsProviderProps} from './types'
+import { createContext, useContext, useState } from 'react';
+import { Steps } from '../screens/QuestionsLayout/types';
+import { IDefaultValues, IQuestionContext, QuestionsProviderProps } from './types';
 
-const QuestionsContext = createContext({} as IQuestionContext)
+const QuestionsContext = createContext({} as IQuestionContext);
 
-export const QuestionsProvider = ( { children }: QuestionsProviderProps) => {
-    const [currentStep, setCurrentStep] = useState<Steps>(
-        Steps.ChooseNumberOFQuestions,
-      );
+export const QuestionsProvider = ({ children }: QuestionsProviderProps) => {
+  const [currentStep, setCurrentStep] = useState<Steps>(
+    Steps.ChooseNumberOFQuestions,
+  );
 
-      const defaultValues :IDefaultValues = {
-          numberofQuestions: '',
-        }
-        
-        const [formValues, setFormValues] =
-        useState<IDefaultValues>(defaultValues);
-        
-    const handleNext = (values: IDefaultValues) => {
-        setFormValues(values);
-       
-        setCurrentStep(currentStep + 1);
-    };
+  const defaultValues :IDefaultValues = {
+    numberofQuestions: '',
+  };
 
-    const handlePrev = () => {
-        setCurrentStep(currentStep - 1);
-    };
+  const [formValues, setFormValues] = useState<IDefaultValues>(defaultValues);
 
-    return (
-        <QuestionsContext.Provider value={{ currentStep, formValues, handleNext, handlePrev }} >
-            {children}
-        </QuestionsContext.Provider>
-    )
-}
+  const handleNext = (values: IDefaultValues) => {
+    setFormValues(values);
+
+    setCurrentStep(currentStep + 1);
+  };
+
+  const handlePrev = () => {
+    setCurrentStep(currentStep - 1);
+  };
+
+  return (
+    <QuestionsContext.Provider value={{
+      currentStep, formValues, handleNext, handlePrev,
+    }}
+    >
+      {children}
+    </QuestionsContext.Provider>
+  );
+};
 
 export const useQuestions = () => {
-    const context = useContext(QuestionsContext)
+  const context = useContext(QuestionsContext);
 
-    return context
-}
+  return context;
+};
